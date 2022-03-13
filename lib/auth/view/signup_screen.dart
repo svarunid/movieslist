9569c518741a913d1328fa04fd458c63bc9e0da0
Arun S V <svarunid@gmail.com>
2022-03-13 16:59:46 +0530

    sign up in progress

--- lib/auth/view/signup_screen.dart
+++ lib/auth/view/signup_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SignUpScreen extends HookWidget {
+import '../../main.dart';
+
+class SignUpScreen extends HookConsumerWidget {
   SignUpScreen({Key? key}) : super(key: key);
 
   final List<String> _profession = ["Doctor", "Engineer", "Teacher", "Student"];
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     TextEditingController _nameController = useTextEditingController();
     TextEditingController _phoneNumberController = useTextEditingController();
     TextEditingController _emailController = useTextEditingController();
@@ -18,6 +21,16 @@ class SignUpScreen extends HookWidget {
     const SizedBox sizedBox = SizedBox(
       height: 14,
     );
+
+    void signUp() {
+      ref.read(authProvider.notifier).signUp(
+          name: _nameController.text,
+          email: _emailController.text,
+          password: _passwordController.text,
+          phoneNumber: _phoneNumberController.text,
+          profession: _selectedVal.value);
+      Navigator.of(context).pop();
+    }
 
     return Scaffold(
       body: Padding(
@@ -93,7 +106,7 @@ class SignUpScreen extends HookWidget {
                 ),
                 sizedBox,
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: signUp,
                   child: const Text("Sign Up"),
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
